03f6783aaedbc0c01b30fdcf3adb9229e91d95c2
Gregory Hawman <g.hawman@gmail.com>
2020-03-19 15:04:20 -0700

    secrets

--- app/src/index.js
+++ app/src/index.js
@@ -23,5 +23,3 @@ rootElement);
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-// XmrmkhzMgz9vGJ19sVn3
